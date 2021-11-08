1e9c639949d5b8ef1aca6e6f5045a3e2e99220e1
estarlyJesus <estarlyjesusamaya@gmail.com>
2021-11-08 16:58:27 -0500

    DOCUMENTACION
    -Se documento el FormsViewModel

--- My Books/app/src/main/java/com/example/mybooks/ViewModel/FormsViewModel.kt
+++ My Books/app/src/main/java/com/example/mybooks/ViewModel/FormsViewModel.kt
@@ -33,6 +33,11 @@ class FormsViewModel : ViewModel() {
         }
     }
 
+    /**INSERTAR UN LIBRO
+     *
+     * @param context
+     * @param data      Lista de strings con el nombre, imagen y descripción de un libro
+     * */
     fun insertBook(context: Context, data: List<String>) {
         if (!data.validateStrings()) {
             "Llena los campos".showToast(context, Toast.LENGTH_SHORT, R.layout.toast_login)
@@ -40,14 +45,14 @@ class FormsViewModel : ViewModel() {
         }
 
         val book = BookEntity(
-            id_book = 0,
-            name = data[0],
-            image = data[1],
-            creation_date = getDateNow(),
-            saved = false,
-            description = data[2],
-            fk_id_user = user.id,
-            lastTimeOpen = ""
+            id_book         = 0,
+            name            = data[0],
+            image           = data[1],
+            creation_date   = getDateNow(),
+            saved           = false,
+            description     = data[2],
+            fk_id_user      = user.id,
+            lastTimeOpen    = ""
         )
 
         println(user.id)
@@ -58,14 +63,20 @@ class FormsViewModel : ViewModel() {
 
     }
 
+    /**ACTUALIZAR UN LIBRO
+     *
+     * @param context
+     * @param data      Lista de strings con el nombre, imagen y descripción de un libro
+     * @param bookOld   Libro el que se quiere actualizar
+     * */
     fun updateBook(context: Context, data: List<String>, bookOld: BookEntity) {
         if (!data.validateStrings()) {
             "Llena los campos".showToast(context, Toast.LENGTH_SHORT, R.layout.toast_login)
             return
         }
-        bookOld.name = data[0]
-        bookOld.image = data[1]
-        bookOld.description = data[2]
+        bookOld.name            = data[0]
+        bookOld.image           = data[1]
+        bookOld.description     = data[2]
 
         GlobalScope.launch(Dispatchers.Main) {
             useCase?.updateBook(bookOld)
@@ -73,16 +84,21 @@ class FormsViewModel : ViewModel() {
 
     }
 
+    /**INSERTAR UN TEMA
+     *
+     * @param context
+     * @param data      Lista de strings con el nombre del tema
+     * */
     fun insertTheme(context: Context, data: List<String>) {
         if (!data.validateStrings()) {
             "Llena los campos".showToast(context, Toast.LENGTH_SHORT, R.layout.toast_login)
             return
         }
         val theme = ThemeEntity(
-            idTheme = data[0].toInt(),
-            name = data[1],
-            importance = if (data[2].toBoolean()) 1 else 0,
-            fk_idBook = data[3].toInt()
+            idTheme     = data[0].toInt(),
+            name        = data[1],
+            importance  = if (data[2].toBoolean()) 1 else 0,
+            fk_idBook   = data[3].toInt()
         )
         useCase = UseCase(context)
         GlobalScope.launch(Dispatchers.Main) {
@@ -92,6 +108,12 @@ class FormsViewModel : ViewModel() {
 
     }
 
+    /**ACTUALIZAR UN TEMA
+     *
+     * @param context
+     * @param data      Lista de strings con el nombre del tema
+     * @param themeOld  Tema que se va a actualizar
+     * */
     fun updateTheme(context: Context, data: List<String>, themeOld: ThemeEntity) {
         if (!data.validateStrings()) {
             "Llena los campos".showToast(context, Toast.LENGTH_SHORT, R.layout.toast_login)
@@ -107,18 +129,27 @@ class FormsViewModel : ViewModel() {
 
     }
 
+    /**OBTENER LA FECHA ACTUAL*/
     fun getDateNow(): String {
         val formater = SimpleDateFormat("dd/M/yyyy")
         return formater.format(Date())
     }
 
+    /**
+     * @param context
+     * @param subTitle Subtitulo del contenido
+     * @param idTheme  Id del tema donde se almacenara
+     * @param data     Lista de TextEntitys
+     * @param view     Lista de TextEntitys
+     * @param type     Para saber en que navigator está (contenidosearch o el principal)
+     * */
     fun insertContent(
-        context: Context,
-        subTitle: String,
-        idTheme: Int,
-        data: List<TextEntity>,
-        view: View,
-        type: String
+        context   :    Context,
+        subTitle  :    String,
+        idTheme   :    Int,
+        data      :    List<TextEntity>,
+        view      :    View,
+        type      :    String
     ) {
         if (subTitle == "") {
             "Ingresa un subtitulo".showToast(
@@ -126,13 +157,12 @@ class FormsViewModel : ViewModel() {
                 Toast.LENGTH_SHORT,
                 R.layout.toast_login
             )
-
         }
 
-        val content = ContentEntity(
-            idContent = 0,
-            subTitle,
-            fk_idTheme = idTheme
+        val content     = ContentEntity(
+            idContent   = 0,
+            subTitle    = subTitle,
+            fk_idTheme  = idTheme
         )
 
 
@@ -141,8 +171,8 @@ class FormsViewModel : ViewModel() {
             data.forEach { data ->
                 if (idContent != null) {
                     if (data.content != "") {
-                        data.fk_id_content = idContent.toInt()
-                        data.id_text = 0
+                        data.fk_id_content  = idContent.toInt()
+                        data.id_text        = 0
                         useCase?.insertTextData(data)
                     }
                 }
